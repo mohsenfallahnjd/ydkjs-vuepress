cc9243271e3edce49ec3a0f9b62126a7232961d3
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2022-08-22 20:50:37 +0430

    Initial commit

--- replace.js
+++ replace.js
@@ -2,7 +2,7 @@
 const replace = require('replace-in-file');
 
 const someProcessingA = (input) => {
-  if (!!input.match(/<img src\=\"[a-zA-Z]/g)) return input.replace(/<img src\=\"/g, '<img src\=\"./');
+  if (!!input.match(/<img.*withBase\('(.*)'\).*alt="([^"]*)".*>/g)) return input.replace(/<img.*withBase\('(.*)'\).*alt="([^"]*)".*>/g, /![$2]($1)/);
   return input;
 }
 const someProcessingB = (input) => input.replace(/https\:\/\/github.com\/getify\/You-Dont-Know-JS\/blob\/2nd-ed\/external-logos\//g, './external-logos/');
