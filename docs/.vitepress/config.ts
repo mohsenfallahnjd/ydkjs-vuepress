f87c8eafd1057a84d26ef25563e32f1e0fa00676
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2022-08-25 14:38:34 +0430

    Enhance readme

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -284,7 +284,6 @@ export default defineConfig({
   lastUpdated: true,
   ignoreDeadLinks: true,
   cleanUrls: 'without-subfolders',
-  base: '/',
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
